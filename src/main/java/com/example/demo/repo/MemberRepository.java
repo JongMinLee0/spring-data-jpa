af1e4b32b1d40151bc5e325d45fd209dcceff6b0
jongminelee0 <jongminlee0@gmail.com>
2020-02-12 17:09:20 +0900

    add : test case

--- src/main/java/com/example/demo/repo/MemberRepository.java
+++ src/main/java/com/example/demo/repo/MemberRepository.java
@@ -2,7 +2,15 @@ package com.example.demo.repo;
 
 import com.example.demo.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByNameAndAndEmail(String name, String email);
+
+    @Query(value = "SELECT m.* FROM MEMBER m WHERE m.email = ?1", nativeQuery = true)
+    Optional<Member> findMemberUseEmailAddress(String email);
 
 }
--- src/test/java/com/example/demo/repo/MemberRepositoryTest.java
+++ src/test/java/com/example/demo/repo/MemberRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.example.demo.repo;
 
 import com.example.demo.entity.Member;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,8 +17,8 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
-
     @Test
+    @Disabled("저장 테스트 완료")
     @DisplayName("Member 저장")
     void memberSaveTest(){
         Member member = new Member();
@@ -25,5 +28,34 @@ class MemberRepositoryTest {
         Member member1 = memberRepository.save(member);
         assertEquals("jongmin", member1.getName());
     }
+
+    @Test
+    @Disabled("조회 테스트 종료")
+    @DisplayName("Member 조회")
+    void findMemberJongminTest(){
+        Optional<Member> member = memberRepository.findById(1L);
+        assertEquals("jongmin", member.get().getName());
+    }
+
+    @Test
+    @DisplayName("메소드 이용")
+    void findMemberByMethod(){
+        String name = "jongmin";
+        String email = "test@test.com";
+        Optional<Member> member = memberRepository.findByNameAndAndEmail(name, email);
+
+        assertEquals(name, member.get().getName());
+        assertEquals(email, member.get().getEmail());
+    }
+
+    @Test
+    @DisplayName("쿼리 이용")
+    void findMemberByEmail(){
+        String email = "test@test.com";
+        Optional<Member> member = memberRepository.findMemberUseEmailAddress(email);
+
+        assertEquals("jongmin", member.get().getName());
+    }
+
 
 }
